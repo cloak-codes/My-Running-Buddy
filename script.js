2af00b7dc10f5bbcb497c446cd57d1e07aeafae6
Geo Cloakey <gcloak4@comcast.net>
2023-04-26 12:16:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Menu
+
+const handleMenu = () => {
+  let menuIcon = document.querySelector(".menu-button");
+  const menu = document.querySelector(".menu");
+
+  let menuOpen = false;
+
+  menuIcon.addEventListener("click", () => {
+    if (!menuOpen) {
+      menu.style.transition = "transform 0.5s ease-out";
+      menu.style.transform = "translateX(-300px)";
+      menuIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+      menuOpen = true;
+    } else {
+      menu.style.transition = "transform 0.5s ease-out";
+      menuIcon.innerHTML = '<i class="fa-solid fa-bars"></i>';
+      menu.style.transform = "translateX(0)";
+      menuOpen = false;
+    }
+  });
+};
+handleMenu();
+
+// Parallax effect
+
+const parallax = () => {
+  const homeText = document.querySelector(".home-text");
+  const bgVideo = document.querySelector(".bg-video");
+
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.pageYOffset;
+
+    homeText.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+    bgVideo.style.transform = "translateY(" + scrollPosition * 0.2 + "px)";
+  });
+};
+
+parallax();
